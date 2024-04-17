62bb52291099eab9f2246956a25ad2cc9bb6e4eb
Iri O <oparina.iri@gmail.com>
2024-04-17 18:56:10 +0500

    Added CustomUser

---
+++ apps/accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from accounts.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'is_staff',
+        'email',
+        'email_confirmed',
+    )
