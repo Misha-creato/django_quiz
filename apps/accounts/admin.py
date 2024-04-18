986f67ac9fc9e3cf55ab9d26338d8468659d3a45
Iri O <oparina.iri@gmail.com>
2024-04-18 16:38:34 +0500

    Changed fields to display

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -5,8 +5,7 @@ from accounts.models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = (
-        'username',
-        'is_staff',
         'email',
+        'is_staff',
         'email_confirmed',
     )
