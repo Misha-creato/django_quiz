1941c36c6732afe2323857ae1a0eee0fc78bb3be
Iri O <oparina.iri@gmail.com>
2024-04-18 16:42:00 +0500

    Changed create_user, created new funcs

--- apps/accounts/services.py
+++ apps/accounts/services.py
@@ -1,43 +1,62 @@
-from django.contrib.auth import get_user_model
-from django.db import DatabaseError
 from django.contrib.auth import authenticate
+from django.contrib import messages
+
 from django.core.mail import send_mail
 from django.urls import reverse
+
 from accounts.tokens import generate_confirmation_token
-from django.contrib.auth.tokens import default_token_generator
-
-User = get_user_model()
+from accounts.models import CustomUser as User
 
 
-def create_user(data):
+def create_user(request, data):
     try:
-        User.objects.create_user(
-            username=data['username'],
+        user = User.objects.create_user(
             email=data['email'],
             password=data['password1'],
         )
-        message = 'Вы успешно зарегистрировались'
-    except DatabaseError as e:
-        print(f'Ошибка базы данных {e}')
-        message = 'Возникла ошибка, пожалуйста, попробуйте позже'
-    return message
+        messages.success(
+            request=request,
+            message='Вы успешно зарегистрировались'
+        )
+        send_confirmation_email(request=request, user=user)
+    except Exception as exc:
+        print(f'Не удалось создать пользователя {exc}')
+        messages.error(
+            request=request,
+            message='Возникла ошибка при создании пользователя, пожалуйста, попробуйте позже'
+        )
 
 
 def send_confirmation_email(request, user):
     subject = 'Подтверждение адреса электронной почты'
-    token = generate_confirmation_token(user=user)
-    url = request.build_absolute_uri(reverse('confirm', args=(user.email, token)))
-    message = f'Привет, {user.username}!\nПожалуйста, подтвердите свой адрес электронной почты, перейдя по ссылке:\n{url}'
-    send_mail(subject, message, 'sanek11konek@gmail.com', [user.email])
+    token = get_user_token(user=user)
+    url = request.build_absolute_uri(reverse('confirm', args=(token,)))
+    message = (
+        f'Привет, {user.username}!\n'
+        f'Пожалуйста, подтвердите свой адрес электронной почты, перейдя по ссылке:\n{url}'
+    )
+    send_mail(subject, message, 'example@gmail.com', [user.email])
 
 
 def get_user(data):
     user = authenticate(
-        username=data['username'],
+        username=data['email'],
         password=data['password'],
     )
     return user
 
 
-def check_token(user, token):
-    return default_token_generator.check_token(user=user, token=token)
+def set_form_error_messages(request, form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            messages.error(
+                request=request,
+                message=error
+            )
+
+
+def get_user_token(user):
+    token = generate_confirmation_token(user=user)
+    user.token = token
+    user.save()
+    return token
