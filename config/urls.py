004d246c0270564c70d77d82e4798e390dbed3aa
Iri O <oparina.iri@gmail.com>
2024-04-19 18:30:48 +0500

    Added survey app urls

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('users/', include('users.urls')),
+    path('survey/', include('survey.urls')),
     path('', include('index.urls')),
 ]
