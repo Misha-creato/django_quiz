c7dfb25de261f14b968ea081a588abf6b8f5671a
Iri O <oparina.iri@gmail.com>
2024-04-19 18:30:29 +0500

    Changed users app configs

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Application definition
 
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+    'users',
     'index',
-    'servey',
+    'survey',
 ]
 
 MIDDLEWARE = [
