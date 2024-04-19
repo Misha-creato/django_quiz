ef8d518d2afc535e55540681466f6002dade3656
Iri O <oparina.iri@gmail.com>
2024-04-19 18:32:15 +0500

    Changed Index view

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Q
+
+from survey.models import Survey
 
 
 class Index(View):
     def get(self, request, *args, **kwargs):
-        return render(request=request, template_name='index.html')
+        user = request.user.id
+        survey = Survey.objects.first()
+        if user:
+            survey = Survey.objects.filter(~Q(options__users=user)).first()
+        context = {
+            'survey': survey
+        }
+        return render(
+            request=request,
+            template_name='index.html',
+            context=context,
+        )
 
     def post(self, request, *args, **kwargs):
+        option = request.POST['option']
+        user = request.user
+        user.options.add(option)
+        survey_stats = Survey.objects.filter(options__id=option).first()
         context = {
-            'response': 'Спасибо, Ваш выбор учтён, Вы можете просмотреть статистику по ссылке'
+            'survey_stats': survey_stats,
         }
-        return render(request=request, template_name='index.html', context=context)
+        return render(
+            request=request,
+            template_name='index.html',
+            context=context,
+        )
