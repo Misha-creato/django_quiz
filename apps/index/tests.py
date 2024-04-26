30449b43d65cc93d3503966719479f785731d821
Iri O <oparina.iri@gmail.com>
2024-04-26 19:10:02 +0500

    Added tests

--- apps/index/tests.py
+++ apps/index/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+from survey.models import (
+    Option,
+    Survey,
+)
+
+
+User = get_user_model()
+
+
+class IndexViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse('index')
+        cls.user = User.objects.create_user(
+            email="test1@example.com",
+            password="password123",
+        )
+
+    def test_index_view_post(self):
+        survey = Survey.objects.create(
+            question='How are you?',
+        )
+        option = Option.objects.create(
+            survey=survey,
+            title='Answer',
+        )
+        data = {
+            'option': option.id,
+        }
+
+        self.client.login(
+            email='test1@example.com',
+            password='password123'
+        )
+        response = self.client.post(
+            path=self.url,
+            data=data,
+        )
+
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Спасибо, Ваш выбор учтён. Вы можете просмотреть статистику в профиле.')
+        self.assertEqual(response.status_code, 302)
